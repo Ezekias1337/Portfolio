a4e2951932a741ba6095f8f3e91305e3d05a04be
Frank Edwards <frankedwards50@gmail.com>
2021-11-12 14:16:51 -0800

    Added animations

--- public/publicJS/main.js
+++ public/publicJS/main.js
@@ -1,39 +1,42 @@
 const checkbox = document.getElementById("switch");
 
 let trans = () => {
-    document.getElementById("body").classList.add("transition");
-    window.setTimeout(() => {
-        document.getElementById("body").classList.remove("transition")
-    }, 550)
-}
+  document.getElementById("body").classList.add("transition");
+  window.setTimeout(() => {
+    document.getElementById("body").classList.remove("transition");
+  }, 550);
+};
 
 checkbox.addEventListener("click", function () {
-    if (checkbox.checked) {
-        trans();
-        document.getElementById("body").setAttribute("data-theme", "dark");
-        let tableArray = document.getElementsByClassName("technology-table");
+  if (checkbox.checked) {
+    trans();
+    document.getElementById("body").setAttribute("data-theme", "dark");
+    let tableArray = document.getElementsByClassName("technology-table");
 
-        for (const item of tableArray) {
-            item.classList.replace("table-light", "table-dark")
-        }
-
-        document.getElementById("navbar").classList.replace("navbar-light", "navbar-dark")
-        document.getElementById("navbar").classList.replace("bg-light", "bg-dark")
-
-    } else {
-        trans();
-        document.getElementById("body").setAttribute("data-theme", "light");
-
-        let tableArray = document.getElementsByClassName("technology-table");
-
-        for (const item of tableArray) {
-            item.classList.replace("table-dark", "table-light")
-        }
-
-        document.getElementById("navbar").classList.replace("navbar-dark", "navbar-light")
-        document.getElementById("navbar").classList.replace("bg-dark", "bg-light")
+    for (const item of tableArray) {
+      item.classList.replace("table-light", "table-dark");
     }
-})
+
+    document
+      .getElementById("navbar")
+      .classList.replace("navbar-light", "navbar-dark");
+    document.getElementById("navbar").classList.replace("bg-light", "bg-dark");
+  } else {
+    trans();
+    document.getElementById("body").setAttribute("data-theme", "light");
+
+    let tableArray = document.getElementsByClassName("technology-table");
+
+    for (const item of tableArray) {
+      item.classList.replace("table-dark", "table-light");
+    }
+
+    document
+      .getElementById("navbar")
+      .classList.replace("navbar-dark", "navbar-light");
+    document.getElementById("navbar").classList.replace("bg-dark", "bg-light");
+  }
+});
 
 //Animated text
 
@@ -43,30 +46,49 @@ const speed = 75;
 const animationDone = false;
 
 function animateTextTyping() {
-    
-    if (i < text.length) {
-        document.getElementById("animated-text").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(animateTextTyping, speed);
-    } else if (i === text.length) {
-        animationDone = true;
-    }
-
+  if (i < text.length) {
+    document.getElementById("animated-text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(animateTextTyping, speed);
+  } else if (i === text.length) {
+    animationDone = true;
+  }
 }
 
 function trailingUnderScore() {
-    let indexForComparison = document.getElementById("animated-text").innerHTML.length - 1;
+  let indexForComparison =
+    document.getElementById("animated-text").innerHTML.length - 1;
 
-    if(document.getElementById("animated-text").innerHTML[indexForComparison] === "_") {
-        let newStr = document.getElementById("animated-text").innerHTML.substring(0, document.getElementById("animated-text").innerHTML.length - 1)
-        document.getElementById("animated-text").innerHTML = newStr;     
-    } else if(document.getElementById("animated-text").innerHTML[indexForComparison] !== "_" && document.getElementById("animated-text").innerHTML[indexForComparison] === "." && document.getElementById("animated-text").innerHTML[indexForComparison - 1] === "." && document.getElementById("animated-text").innerHTML[indexForComparison - 2] === ".") {
-            document.getElementById("animated-text").innerHTML = document.getElementById("animated-text").innerHTML + "_";
-    } 
+  if (
+    document.getElementById("animated-text").innerHTML[indexForComparison] ===
+    "_"
+  ) {
+    let newStr = document
+      .getElementById("animated-text")
+      .innerHTML.substring(
+        0,
+        document.getElementById("animated-text").innerHTML.length - 1
+      );
+    document.getElementById("animated-text").innerHTML = newStr;
+  } else if (
+    document.getElementById("animated-text").innerHTML[indexForComparison] !==
+      "_" &&
+    document.getElementById("animated-text").innerHTML[indexForComparison] ===
+      "." &&
+    document.getElementById("animated-text").innerHTML[
+      indexForComparison - 1
+    ] === "." &&
+    document.getElementById("animated-text").innerHTML[
+      indexForComparison - 2
+    ] === "."
+  ) {
+    document.getElementById("animated-text").innerHTML =
+      document.getElementById("animated-text").innerHTML + "_";
+  }
 }
 
 function intervalForCurson() {
-    setInterval(trailingUnderScore, 500);
+  setInterval(trailingUnderScore, 1000);
 }
 
 animateTextTyping();
